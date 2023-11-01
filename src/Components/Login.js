475879a04aa59497e74ea1dbcc95182fab6338cd
Thejaswini-B <tejaswini.bandaru@ro-one.in>
2023-11-01 17:05:09 +0530

    first commit

---
+++ src/Components/Login.js
@@ -0,0 +1,113 @@
+import React, { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import axios from "axios";
+
+function Login() {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (email !== "" && password !== "") {
+      const body = {
+        email: email,
+        password: password,
+      };
+
+      try {
+        const response = await axios.post("http://localhost:3007/signin", body);
+
+        // Successfully logged in
+        if (response.status === 200 && response.data.jwtToken) {
+          alert("Login successful");
+          navigate("/allvideos");
+        } else {
+          alert("Unexpected error occurred. Please try again later.");
+        }
+      } catch (error) {
+        console.log(error);
+
+        // Handle specific error messages from the server
+        if (error.response) {
+          if (
+            error.response.status === 400 &&
+            error.response.data === "Invalid Password"
+          ) {
+            alert("Invalid Password. Please try again.");
+          } else if (
+            error.response.status === 400 &&
+            error.response.data === "Email Doesn't Exists"
+          ) {
+            alert("Email Doesn't Exist. Please check and try again.");
+          } else {
+            alert("Error occurred during login. Please try again.");
+          }
+        } else {
+          alert("Unexpected error occurred. Please try again.");
+        }
+      }
+    } else {
+      alert("Please enter email and password.");
+    }
+  };
+
+  return (
+    <div className="bg-cont">
+      <div
+        style={{ padding: 110 }}
+        className="d-flex flex-row justify-content-center"
+      >
+        <div className="form-container">
+          <center className="middle">Login Here</center>
+          <form className="form" onSubmit={handleSubmit}>
+            <label>Email</label>
+            <input
+              type="email"
+              className="input"
+              placeholder="Enter Your Email"
+              value={email}
+              required
+              onChange={(e) => {
+                handleEmailChange(e);
+              }}
+            />
+            <label>Password</label>
+            <input
+              type="password"
+              className="input"
+              placeholder="Password"
+              value={password}
+              required
+              onChange={(e) => {
+                handlePasswordChange(e);
+              }}
+            />
+            <center>
+              {" "}
+              <button>Login</button>
+            </center>
+
+            <span style={{ paddingBottom: 55, paddingTop: 25 }}>
+              Don't Have An Account?
+              <Link to="/signup">
+                <span style={{ color: "#0D6EFD" }}> Sign Up</span>
+              </Link>
+            </span>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
---
+++ src/Components/SignUp.js
@@ -0,0 +1,157 @@
+import React, { useState } from 'react';
+import {useNavigate,Link} from "react-router-dom" ;
+import axios from "axios" ;
+
+function SignUp() {
+  const navigate = useNavigate();
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState(''); 
+  const [errors, setErrors] = useState([]);
+  const [gender, setGender] = useState('');
+  
+
+
+const validatePassword = () => {
+  const isValid =
+    /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*(),.?":{}|<>]).{8,}$/.test(
+      password
+    );
+
+  if (!isValid) {
+    setErrors([
+      "Password must be at least 8 characters, contain at least one alphabet, one digit, and one special character.",
+    ]);
+  } else {
+    setErrors([]);
+  }
+};
+
+  const handleChange = (e) => {
+    setName(e.target.value);
+  }
+ 
+
+  const handleGenderChange = (e) => {
+    setGender(e.target.value);
+  }
+ 
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  }
+  
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  }
+ 
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+ 
+    
+    // checking for non-empty fields
+    if (name !== '' && email !== '' && password !== '' && gender !== '' ) {
+       const body = {
+          "name": name ,
+          "email": email ,
+          "password": password ,
+          "gender": gender
+
+  }
+      try {
+        const response = await axios.post('http://localhost:3007/signup', body);
+        console.log(response.data);
+        alert('signup successful')
+        setName('')
+        setEmail('')
+        setPassword('')
+        setGender('')
+        navigate('/login');
+      } catch (error) {
+        console.log(error);
+        alert('signup failed')
+      }
+    }
+  };
+  return (
+    <div className="bg-cont">
+      <div
+        style={{ padding: 60 }}
+        className="d-flex flex-row justify-content-center"
+      >
+        <div className="form-container-signup">
+          <center className="middle-signup">Create Account</center>
+          <form className="form-signup" onSubmit={handleSubmit}>
+            <label>Name</label>
+            <input
+              type="text"
+              className="input-signup"
+              placeholder="Enter Your Name"
+              value={name}
+              onChange={(e) => {
+                handleChange(e);
+              }}
+              required
+            />
+            <label>Email</label>
+            <input
+              type="email"
+              className="input-signup"
+              placeholder="Enter Your Email"
+              value={email}
+              required
+              onChange={(e) => {
+                handleEmailChange(e);
+              }}
+            />
+            <label>Password</label>
+            <input
+              type="password"
+              onBlur={validatePassword}
+              className="input-signup"
+              placeholder="Password"
+              value={password}
+              required
+              onChange={(e) => {
+                handlePasswordChange(e);
+              }}
+            />
+            <ul>
+              {errors.map((error, index) => (
+                <li key={index} style={{ color: "red" }}>
+                  {error}
+                </li>
+              ))}
+            </ul>
+            <label>Gender</label>
+            <input
+              type="text"
+              className="input-signup"
+              placeholder="Gender"
+              value={gender}
+              required
+              onChange={(e) => {
+                handleGenderChange(e);
+              }}
+            />
+            <center>
+              {" "}
+              <button>Sign Up</button>{" "}
+            </center>
+
+            <span style={{ paddingBottom: 35, paddingTop: 10 }}>
+              Already Have An Account?
+              <Link to="/login">
+                <span style={{ color: "#0D6EFD" }}> Login </span>
+              </Link>
+            </span>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default SignUp;
